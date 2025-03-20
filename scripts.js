f77313f5b90114ff83b08e76368578ea14827312
BenSuzarte <ben.dantassuzarte@gmail.com>
2025-03-19 21:32:13 -0300

    finish

--- scripts.js
+++ scripts.js
@@ -1,30 +1,55 @@
 class Pessoa {
-    constructor(nome, idade, altura) {
+    constructor(nome, idade, altura, img) {
         this.nome = nome
         this.idade = idade
         this.altura = altura
+        this.img = img
     }
 }
 
-const pessoas = []
+let pessoas = []
 
 function AoClicarNoInput(input, pessoa) {
 
+    // O usuário deselecionou o input?
+    if(input.checked == false) {
+
+        const pessoaIndex = PosicaoDaPessoaNaLista(pessoa)
+        pessoas.splice(pessoaIndex, 1)
+
+        // pessoas = pessoas.filter(obj => obj.nome !== pessoa.nome)
+        return
+    }
+
     // Selecionou mais de duas pessoas?
     if(pessoas.length > 1) {
-        input.checked = 0
+        input.checked = false
         alert("Não pode selecionar mais de duas pessoas")
+
+        return
     }
 
     pessoas.push(pessoa)
 
 }
 
-function ExibirComparacao() {
-
+function PosicaoDaPessoaNaLista(pessoa) {
+    for(let i = 0; i < pessoas.length; i++){
+        if(pessoas[i].nome === pessoa.nome) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 function AtualizarDados() {
+
+    // Adiciona a foto na tabela
+    const imgUm = document.getElementById("pessoa-imagem-1")
+    const imgDois = document.getElementById("pessoa-imagem-2")
+
+    imgUm.src = pessoas[0].img
+    imgDois.src = pessoas[1].img
     
     // Adiciona nome na tabela
     const tdNomeUm = document.getElementById("pessoa-nome-1")
@@ -48,4 +73,23 @@ function AtualizarDados() {
 
     tdAlturaUm.innerHTML = pessoas[0].altura
     tdAlturaDois.innerHTML = pessoas[1].altura
+}
+
+function ExibirComparacao() {
+
+    if(pessoas.length !== 2) {
+        alert("O número de pessoas tem que ser dois")
+        return
+    }
+
+    AtualizarDados()
+    
+    const dialog = document.getElementById("mostrar-tabela")
+
+    dialog.showModal()
+}
+
+function EsconderComparacao() {
+    const dialog = document.getElementById("mostrar-tabela")
+    dialog.close()
 }
